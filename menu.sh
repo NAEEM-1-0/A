7bfeadbe94a31b05bd5d3bc5b4703f1072627e40
NAEEM <138077182+NAEEM-1-0@users.noreply.github.com>
2026-01-12 17:37:17 +0600

    menu.sh

--- menu.sh
+++ menu.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-# Force input from terminal (fixes auto-run)
-exec </dev/tty
-
-BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
-CD_DIR="$BASE_DIR/cd"
-
-# Root check
-[ "$EUID" -ne 0 ] && echo "Run as root" && exit 1
+CD_DIR="$(pwd)/cd"
 
 while true; do
   echo
-  echo "1) panel"
-  echo "2) wings"
-  echo "3) update"
-  echo "4) blueprint"
-  echo "5) cloudflare"
-  echo "6) theme"
-  echo "7) uninstall"
-  echo "8) ssh"
-  echo "9) system info"
-  echo "0) exit"
+  echo "1 panel"
+  echo "2 wings"
+  echo "3 update"
+  echo "4 blueprint"
+  echo "5 cloudflare"
+  echo "6 theme"
+  echo "7 uninstall"
+  echo "0 exit"
   echo
 
-  read -r choice
+  read choice
 
   case "$choice" in
     1) bash "$CD_DIR/panel.sh" ;;
@@ -33,9 +24,7 @@ while true; do
     5) bash "$CD_DIR/cloudflare.sh" ;;
     6) bash "$CD_DIR/th.sh" ;;
     7) bash "$CD_DIR/uninstalll.sh" ;;
-    8) bash "$CD_DIR/root2.sh" ;;
-    9) bash "$CD_DIR/root" ;;
     0) exit ;;
-    *) echo "invalid option" ;;
+    *) echo "invalid" ;;
   esac
 done
